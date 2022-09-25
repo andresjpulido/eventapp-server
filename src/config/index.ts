d582d5aad538e38fcc96c9d128e16dbaec54c09b
Andres <andresjpulido@gmail.com>
2022-09-25 17:30:26 +1300

    adding new mongo parameters

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,7 @@ export default {
 	port: process.env.PORT,	 
 	MONGODB_USER: process.env.MONGODB_USER,
 	MONGODB_HOST: process.env.MONGODB_HOST,
-	MONGODB_DBNAME:process.env.MONGODB_DBNAME
-	 
+	MONGODB_DBNAME:process.env.MONGODB_DBNAME,
+	MONGODB_PASSWORD:process.env.MONGODB_PASSWORD,
+	MONGODB_PORT:process.env.MONGODB_PORT
 };
--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,11 +3,11 @@ import model from '../config';
 
 export default async function connect() {
 	try {
-		await mongoose.connect(`mongodb://${model.MONGODB_HOST}/${model.MONGODB_DBNAME}`, {});
+		await mongoose.connect(`mongodb://${model.MONGODB_USER}:${model.MONGODB_PASSWORD}@${model.MONGODB_HOST}:${model.MONGODB_PORT}/${model.MONGODB_DBNAME}`, {});
 		console.log("DB Connected")
 		//mongoose.connection.db.dropDatabase();
 		 
 	} catch (error) {
-		console.log("Error connecting to DB", error.description);
+		console.log("Error connecting to DB", error);
 	}
 }
